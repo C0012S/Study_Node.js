cc8545a1e5d2f87ae073817c13b4e82031a79be7
C0012S <qkrtkd1002@duksung.ac.kr>
2022-01-22 21:21:42 +0900

    ch03-01 자바스크립트의 변수와 객체

---
+++ NodeExample/ch03_test1.js
@@ -0,0 +1,10 @@
+var name; // 자바스크립트의 변수는 var을 붙인다. // name이라는 변수를 만든다. // 이렇게 바로 세미콜론을 붙이면 변수 선언만 하는 것이다.
+console.log('name : ' + name); // name은 undefined가 된다. 값이 아무것도 할당되지 않았다.
+
+var age = 20; // 변수 선언하면서 초기화
+console.log('age : ' + age); // age는 처음에 초기화가 되었다.
+
+name = '소녀시대'; // 한 번 선언된 name에 값을 그 다음에 할당할 수 있다.
+console.log('name : ' + name);
+
+// age, name : 변수  // 변수 앞에 var을 붙인다.  // 할당이라고 하는 건 =을 붙인다.
