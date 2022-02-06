0bbd11da1206a0216cb990e676d6a978dbb84af4
C0012S <qkrtkd1002@duksung.ac.kr>
2022-02-07 01:47:24 +0900

    ch06-08 몽구스로 데이터베이스 다루기 - 2

--- DatabaseExample/app3.js
+++ DatabaseExample/app3.js
@@ -11,7 +11,7 @@ var expressSession = require('express-session');
 var expressErrorHandler = require('express-error-handler');
 
 // mongoose 모듈 사용
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'); // npm install mongoose --save로 외장 모듈 설치
 
 var database;
 var UserSchema; // UserSchema를 다른 데에서도 쓰고 싶어 위에서 먼저 선언한다.
@@ -159,9 +159,8 @@ app.use('/', router);
 var authUser = function(db, id, password, callback) {
     console.log('authUser 호출됨 : ' + id + ', ' + password);
     
-    var users = db.collection('users');
     
-    users.find({"id":id, "password":password}).toArray(function(err, docs) {
+    UserModel.find({"id":id, "password":password}, function(err, docs) {
         if (err) {
             callback(err, null);
             return;
@@ -175,29 +174,24 @@ var authUser = function(db, id, password, callback) {
             console.log('일치하는 사용자를 찾지 못함.');
             callback(null, null);
         }
-    });
+    }); // mongoDB 사용할 때와의 차이점 : users collection을 참조하는 게 아니라 UserModel에서 find를 한다.
 };
 
 
 var addUser = function(db, id, password, name, callback) {
     console.log('addUser 호출됨 : ' + id + ', ' + password + ', ' + name);
+        
     
-    var users = db.collection('users');
+    var user = new UserModel({"id":id, "password":password, "name":name}); // new 객체 생성 방식을 사용해서 UserModel이 프로토타입으로 동작
     
-    users.insertMany([{"id":id, "password":password, "name":name}], function(err, result) {
+    user.save(function(err) {
         if (err) {
             callback(err, null);
             return;
         }
         
-        if (result.insertedCount > 0) {
-            console.log('사용자 추가됨 : ' + result.insertedCount);
-            callback(null, result);
-        }
-        else {
-            console.log('추가된 레코드가 없음.');
-            callback(null, null);
-        }
+        console.log('사용자 데이터 추가함.');
+        callback(null, user); // user 객체 안에 정보가 들어 있을 것이다.
     });
 };
 
@@ -219,3 +213,9 @@ var server = http.createServer(app).listen(app.get('port'), function() {
     
     connectDB();
 });
+
+// mongoose를 이용해서, 로그인을 위해서 해당하는 아이디와 패스워드가 정상적으로 매칭되는 게 있는지 확인하고 로그인이 정상적으로 됐는지와 안 됐는지를 구분하는 것과 데이터를 추가하는 방법을 봤다.
+// UserSchema와 UserModel을 사용한다. UserSchema는 테이블을 정의하는 것과 비슷하다. UserModel에서는 데이터를 조작하는데, 새로운 객체를 추가할 때는 new UserModel을 이용해서 새로운 객체를 만든 다음에 그거로 save를 한다. // UserModel에 find를 하는 방법과 조금 다르긴 하지만, 그런 방법들을 사용할 수 있다.
+// mongoose로 대체해 봤다.
+// mongodb 모듈을 사용하면 그 안에 다양한 형태의 객체가 들어갈 수 있는데, 그것도 document(문서) 객체 안에 들어가는 데이터의 속성을 정해 놓고 쓰면 문제는 없다. 그런데 mongoose가 여러 가지 다양한 다른 기능을 더 추가해서 제공할 수 있으므로 mongoose를 쓰는 것도 나쁘지 않다.
+// 추가로 더 스키마를 만들 때, index를 정의하거나 또는 메소드를 정의해서 편리하게 사용하는 방법을 같이 볼 것이다.
