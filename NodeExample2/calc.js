a546dabc9c73f76a1333d80cfd6bf30eeabddf13
C0012S <qkrtkd1002@duksung.ac.kr>
2022-02-24 22:45:55 +0900

    [복습] ch02-03 노드에서 모듈 사용하기 - 1(모듈로 분리하기 전 & 모듈로 분리한 후 exports 사용)

---
+++ NodeExample2/calc.js
@@ -0,0 +1,4 @@
+// 계산기 기능의 더하기 함수 정의
+exports.add = function(a, b) {
+    return a + b;
+}; // exports 안에 속성 add가 들어 있다.
---
+++ NodeExample2/ch02_test4.js
@@ -0,0 +1,8 @@
+// 계산기 기능 - 계산기 객체를 만들고 그 안에 속성을 넣는다.
+var calc = {};
+
+calc.add = function(a, b) {
+    return a + b;
+}; // calc 객체의 속성을 정의
+
+console.log('모듈로 분리하기 전 - calc.add : ' + calc.add(10, 10));
