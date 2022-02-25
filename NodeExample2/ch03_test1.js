9d3d5f5d2fdb1673442480bb0a9250c08a57c6fb
C0012S <qkrtkd1002@duksung.ac.kr>
2022-02-25 22:18:27 +0900

    [복습] ch03-01 자바스크립트의 객체와 함수 이해하기 - 1

---
+++ NodeExample2/ch03_test1.js
@@ -0,0 +1,11 @@
+// 3 장 - 자바스크립트 정리
+var name; // 자바스크립트의 변수는 var을 붙인다. // 변수 선언
+console.log('name : ' + name);
+
+var age = 20; // 변수를 선언하면서 초기화 한다.
+console.log('age : ' + age);
+
+name = '소녀시대'; // 값 할당
+console.log('name : ' + name);
+
+// name, age는 변수 // 변수 앞에 var을 붙인다. // 할당은 =을 붙인다.
